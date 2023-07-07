a4869141394ad8b75fe59bfd5efc45c99c665efe
lexunok <lex48949@gmail.com>
2023-07-07 17:30:16 +0500

    register form

--- practice/testing_system/forms.py
+++ practice/testing_system/forms.py
@@ -3,13 +3,15 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
-from .models import Student
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'field','placeholder':'Имя пользователя'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'field','placeholder':'Пароль'}))
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'field', 'placeholder': 'Имя пользователя'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'field', 'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'field', 'placeholder': 'Пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'field', 'placeholder': 'Повторите пароль'}))
     class Meta:
         model = User
         fields = [
